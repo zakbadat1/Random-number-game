ff6a4c920df2313ce8f48d74b3e7de29cd3ba2d0
zakbadat1 <zakariya.badat@ada.ac.uk>
2022-11-04 12:43:23 +0000

    dif_from_answer

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import random
+
 num = 0
 def generate_random_number():
-  num = random.randint(1,100)
+  num = random.randint(1,10)
   print(num)
-generate_random_number()
+
   
+def difference_from_answer(guess, answer):
+  if guess == answer:
+    return "Correct"
+  elif guess > answer:
+    return "Too high"
+  else:
+    return "Too Low"
+
+
+
